68dc0a718efc6e18b2db9b89a8e31a20ee199faf
ifilot <ivo@ivofilot.nl>
2026-02-01 18:14:16 +0100

    Updating README.md

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -251,7 +251,7 @@ MainWindow::MainWindow(const std::shared_ptr<QStringList> _log_messages,
 
     // set Window properties
     this->setWindowTitle(QString(PROGRAM_NAME) + " " + QString(PROGRAM_VERSION));
-    this->resize(1280,960);
+    this->resize(800,600);
 
     qDebug() << "Done building MainWindow";
 }
